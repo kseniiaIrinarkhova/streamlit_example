9bbdc1bb915d598b6b03d200e9071ea2c3065df4
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2024-12-03 18:53:44 -0800

    main layout of chat page

--- pages/chat.py
+++ pages/chat.py
@@ -16,3 +16,23 @@ if 'default_model' not in st.session_state:
 if 'messages' not in st.session_state:
     st.session_state['messages'] = []
     print(st.session_state)
+
+# Sidebar
+st.sidebar.title('Chat')
+
+# Main page
+st.title('Chat Page')
+st.write('Chatbot Powered by Groq and llama3-8b-8192 default model')
+st.divider()
+
+# display list of messages
+if st.session_state.messages:
+    for message in st.session_state.messages:
+        with st.chat_message(message['role']):
+            st.write(message['content'])
+else:
+    st.write('No messages yet')
+
+
+st.divider()
+#input for new messages
