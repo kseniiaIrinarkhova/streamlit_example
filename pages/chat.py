e4878d6ed640d5ee7f5d41ed904fe008704a9252
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2024-12-03 18:45:37 -0800

    set default model

--- pages/chat.py
+++ pages/chat.py
@@ -4,3 +4,10 @@ from groq import Groq
 
 # initializing groq
 client = Groq(api_key=st.secrets['GROQ_API_KEY'])
+
+print(st.session_state)
+
+# set default model
+if 'default_model' not in st.session_state:
+    st.session_state['default_model'] = 'llama3-8b-8192'
+    print(st.session_state)
